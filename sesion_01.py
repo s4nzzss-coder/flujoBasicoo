f73a9b3fda3d45646dfbebbdaa37561b577298e0
s4nzzss <s4nzzss@gmail.com>
2026-02-12 18:14:30 -0800

    guarda mi inicio de sesion en phyton basico

---
+++ sesion_01.py
@@ -0,0 +1,14 @@
+# Numerico y flotante
+print(int(7))
+print(float(7.7))
+print(type(7))
+print(type(7.77))
+print (int(1+2))
+print(int(10*2))
+print ("=== Operadores matematicos====")
+# +
+# -
+# *
+# /
+# **
+# % modulo 
